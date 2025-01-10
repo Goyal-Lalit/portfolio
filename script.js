c4b5e4a5dfac765b43c04bec8a095c8b8022bd0d
lalit goyal <136834573+Goyal-Lalit@users.noreply.github.com>
2025-01-11 00:48:46 +0530

    Update script.js
    Signed-off-by: lalit goyal <136834573+Goyal-Lalit@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // locomovie code
-// gsap.registerPlugin(ScrollTrigger);
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector(".scrollbar"),
-//   smooth: true
-// });
+gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".scrollbar"),
+  smooth: true
+});
 
-// ScrollTrigger.scrollerProxy(".scrollbar", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
+ScrollTrigger.scrollerProxy(".scrollbar", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
 
-//   pinType: document.querySelector(".scrollbar").style.transform ? "transform" : "fixed"
-// });
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-// ScrollTrigger.refresh();
+  pinType: document.querySelector(".scrollbar").style.transform ? "transform" : "fixed"
+});
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
 
 
 //cursor
